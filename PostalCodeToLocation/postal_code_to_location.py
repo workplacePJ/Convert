b232f9a8921b4bb7ebf4a7d78476842782690996
workplacePJ <workplace.pj@proton.me>
2024-08-06 07:05:47 +0900

    postal_code_to_location.py の更新

--- PostalCodeToLocation/postal_code_to_location.py
+++ PostalCodeToLocation/postal_code_to_location.py
@@ -171,6 +171,8 @@ async def convert_postal_code_to_location(session, postal_code: str, **kwargs) -
                         if "city" in item['fullWidthKana']:
                             result_object['kana']['city'] = item['fullWidthKana'].get('city')
                         if "town" in item['fullWidthKana']:
+                            if pattern__further_divisions.search(item['fullWidthKana'].get('town')):
+                                result_object['kana']['further_divisions'] = pattern__further_divisions.search(item['fullWidthKana'].get('town')).group()
                             result_object['kana']['suburb'] = item['fullWidthKana'].get('town')
                         if 
                             result_object['kana']['further_divisions'] = item['fullWidthKana'].get('')
