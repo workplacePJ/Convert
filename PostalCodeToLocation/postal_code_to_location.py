15187ec70af2ed32ebec069f8ef6e975b4731479
workplacePJ <workplace.pj@proton.me>
2024-08-05 17:25:57 +0900

    postal_code_to_location.py の更新

--- PostalCodeToLocation/postal_code_to_location.py
+++ PostalCodeToLocation/postal_code_to_location.py
@@ -180,6 +180,7 @@ async def convert_postal_code_to_location(session, postal_code: str, **kwargs) -
                     if "ja" in result_object:
                         if "further_divisions" in result_object['ja']:
                             if "location" in item:
+                                if "latitude" in item['location'] or "longitude" in item['location']:
                                 result_object['location'] = {}
                                 if "latitude" in item['location']:
                                     result_object['location']['lat'] = item['location'].get('latitude')
