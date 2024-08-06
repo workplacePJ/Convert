9d4e1e170448e39c41b581ec3f6f1ce795d46f5a
workplacePJ <workplace.pj@proton.me>
2024-08-06 10:21:42 +0900

    Update postal_code_to_location.py

--- PostalCodeToLocation/postal_code_to_location.py
+++ PostalCodeToLocation/postal_code_to_location.py
@@ -54,72 +54,77 @@ async def convert_postal_code_to_location(session, postal_code: str, **kwargs) -
 
                 response.raise_for_status()
                 data: Any = await response.json()
-                
-                result['status_code'] = response.status
-                result['is_success'] = True
-                result['convert_from'] = "postal_code"
-                result['requested_value'] = postal_code
-                result['results'] = []
 
-                pattern__further_divisions: Pattern[str] = re.compile(r'([0-9０-９-－]+)$')
-                
-                for item in data:
+                if data:
                     
-                    # generate "result object"data
-                    result_object: dict[str, Any] = {}
-
-                    # Assign value to "result object"data
-                    if "postcode" in item:
-                        result_object['postal_code'] = item.get('postcode')
-                    if "prefCode" in item:
-                        result_object['prefecture_code'] = item.get('prefCode')
+                    result['status_code'] = response.status
+                    result['is_success'] = True
+                    result['convert_from'] = "postal_code"
+                    result['requested_value'] = postal_code
+                    result['results'] = []
+    
+                    pattern__further_divisions: Pattern[str] = re.compile(r'([0-9０-９-－]+)$')
                     
-                    result_object['ja'] = {}
-                    if "pref" in item:
-                        result_object['ja']['prefecture'] = item.get('pref')
-                    if "city" in item:
-                        result_object['ja']['city'] = item.get('city')
-                    if "town" in item:
-                        if pattern__further_divisions.search(item.get('town')):
-                            result_object['ja']['suburb'] = item.get('town').replace(pattern__further_divisions.search(item.get('town')).group(), '')
-                            result_object['ja']['further_divisions'] = pattern__further_divisions.search(item.get('town')).group()
-                        else:
-                            result_object['ja']['suburb'] = item.get('town')
-                    if "office" in item:
-                        result_object['ja']['enterprise_place'] = item.get('office')
-                    if "allAddress" in item:
-                        result_object['ja']['full_address'] = item.get('allAddress')
-
-                    if "fullWidthKana" in item:
-                        result_object['kana'] = {}
-                        if "pref" in item['fullWidthKana']:
-                            result_object['kana']['prefecture'] = item['fullWidthKana'].get('pref')
-                        if "city" in item['fullWidthKana']:
-                            result_object['kana']['city'] = item['fullWidthKana'].get('city')
-                        if "town" in item['fullWidthKana']:
-                            if pattern__further_divisions.search(item['fullWidthKana'].get('town')):
-                                result_object['kana']['suburb'] = item['fullWidthKana'].get('town').replace(pattern__further_divisions.search(item['fullWidthKana'].get('town')).group(), '')
-                                result_object['kana']['further_divisions'] = pattern__further_divisions.search(item['fullWidthKana'].get('town')).group()
+                    for item in data:
+                        
+                        # generate "result object"data
+                        result_object: dict[str, Any] = {}
+    
+                        # Assign value to "result object"data
+                        if "postcode" in item:
+                            result_object['postal_code'] = item.get('postcode')
+                        if "prefCode" in item:
+                            result_object['prefecture_code'] = item.get('prefCode')
+                        
+                        result_object['ja'] = {}
+                        if "pref" in item:
+                            result_object['ja']['prefecture'] = item.get('pref')
+                        if "city" in item:
+                            result_object['ja']['city'] = item.get('city')
+                        if "town" in item:
+                            if pattern__further_divisions.search(item.get('town')):
+                                result_object['ja']['suburb'] = item.get('town').replace(pattern__further_divisions.search(item.get('town')).group(), '')
+                                result_object['ja']['further_divisions'] = pattern__further_divisions.search(item.get('town')).group()
                             else:
-                                result_object['kana']['suburb'] = item['fullWidthKana'].get('town')
-                        if "office" in item['fullWidthKana']:
-                            result_object['kana']['enterprise_place'] = item['fullWidthKana'].get('office')
-                        if "allAddress" in item['fullWidthKana']:
-                            result_object['kana']['full_address'] = item['fullWidthKana'].get('allAddress')
+                                result_object['ja']['suburb'] = item.get('town')
+                        if "office" in item:
+                            result_object['ja']['enterprise_place'] = item.get('office')
+                        if "allAddress" in item:
+                            result_object['ja']['full_address'] = item.get('allAddress')
+    
+                        if "fullWidthKana" in item:
+                            result_object['kana'] = {}
+                            if "pref" in item['fullWidthKana']:
+                                result_object['kana']['prefecture'] = item['fullWidthKana'].get('pref')
+                            if "city" in item['fullWidthKana']:
+                                result_object['kana']['city'] = item['fullWidthKana'].get('city')
+                            if "town" in item['fullWidthKana']:
+                                if pattern__further_divisions.search(item['fullWidthKana'].get('town')):
+                                    result_object['kana']['suburb'] = item['fullWidthKana'].get('town').replace(pattern__further_divisions.search(item['fullWidthKana'].get('town')).group(), '')
+                                    result_object['kana']['further_divisions'] = pattern__further_divisions.search(item['fullWidthKana'].get('town')).group()
+                                else:
+                                    result_object['kana']['suburb'] = item['fullWidthKana'].get('town')
+                            if "office" in item['fullWidthKana']:
+                                result_object['kana']['enterprise_place'] = item['fullWidthKana'].get('office')
+                            if "allAddress" in item['fullWidthKana']:
+                                result_object['kana']['full_address'] = item['fullWidthKana'].get('allAddress')
+                        
+                        if "ja" in result_object:
+                            if "further_divisions" in result_object['ja']:
+                                if "location" in item:
+                                    if "latitude" in item['location'] or "longitude" in item['location']:
+                                        result_object['location'] = {}
+                                        if "latitude" in item['location']:
+                                            result_object['location']['lat'] = item['location'].get('latitude')
+                                        if "longitude" in item['location']:
+                                            result_object['location']['lng'] = item['location'].get('longitude')
+                        
+                        result['results'].append(result_object)
+                else:
                     
-                    if "ja" in result_object:
-                        if "further_divisions" in result_object['ja']:
-                            if "location" in item:
-                                if "latitude" in item['location'] or "longitude" in item['location']:
-                                    result_object['location'] = {}
-                                    if "latitude" in item['location']:
-                                        result_object['location']['lat'] = item['location'].get('latitude')
-                                    if "longitude" in item['location']:
-                                        result_object['location']['lng'] = item['location'].get('longitude')
+                    raise Http404
                     
-                    result['results'].append(result_object)
-
-        # error handling\n","
+        # error handling","
         except aiohttp.ClientError as err:
             result['status_code'] = response.status
             result['is_success'] = False
