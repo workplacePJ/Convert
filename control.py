32b6e6896dc831930b4cf178e5eb5b38921b7534
workplacePJ <workplace.pj@proton.me>
2024-08-06 15:24:14 +0900

    Update control.py

--- control.py
+++ control.py
@@ -1,4 +1,5 @@
 from PostalCodeToLocation.postal_code_to_location import convert_postal_code_to_location
+from LocationToPostalCode.location_to_postal_code import convert_location_to_postal_code
 from typing import Any
 import aiohttp
 import asyncio
@@ -13,11 +14,11 @@ async def control(requested_values: list[dict[str, str]], **kwargs) -> list[dict
                 value: str = requested_value.get('postal_code')
                 tasks.append(convert_postal_code_to_location(session, value, KEY = kwargs.get('POSTCODE_JP_API_KEY', '')))
                 await asyncio.sleep(2)
-            """
+            
             elif "address" in requested_value:
                 value: str = requested_value.get('address')
                 tasks.append(convert_location_to_postal_code(session, "address", value, KEY = kwargs.get('GOOGLE_MAPS_API_KEY', '')))
-
+            """
             elif "landmark" in requested_value:
                 value: str = requested_value.get('landmark')
                 tasks.append(convert_location_to_postal_code(session, "landmark", value))
